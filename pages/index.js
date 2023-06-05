330cde8b934c9867b4965097cf86e542e6e7521c
Felipe Soller <soller@outlook.com>
2023-06-04 21:45:43 -0300

    adds buttons to choose display format

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
+import { FiList, FiGrid } from 'react-icons/fi';
+
 import { fetchGenreLists } from '@/api/nytApi';
+
+import GenreCardList from '@/components/GenreCardList';
 import GenreList from '@/components/GenreList';
 
 import 'tailwindcss/tailwind.css';
 
 export default function Home({ genreLists }) {
+  const [displayMode, setDisplayMode] = useState('list');
+
+  const handleDisplayMode = (mode) => {
+    setDisplayMode(mode);
+  };
+
   return (
-    <GenreList genreLists={genreLists} />
+    <div>
+      <div className="flex flex-col sm:flex-row justify-between items-center bg-slate-100 p-2 px-5 sm:px-28">
+        <div className="flex justify-between items-center mt-2 sm:mt-0">
+          <div className="flex">
+            <button
+              onClick={() => handleDisplayMode('list')}
+              className={`p-2 ml-2 rounded-lg ${displayMode === 'list' ? 'text-royalBlue' : ''}`}
+            >
+              <FiList />
+            </button>
+            <button
+              onClick={() => handleDisplayMode('card')}
+              className={`p-2 ml-2 rounded-lg ${displayMode === 'card' ? 'text-royalBlue' : ''}`}
+            >
+              <FiGrid />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 pl-28 pr-6">
+        {displayMode === 'list' ? (
+          <GenreList genreLists={genreLists} />
+        ) : (
+          <GenreCardList genreLists={genreLists} />
+        )}
+      </div>
+    </div>
   );
 }
 
