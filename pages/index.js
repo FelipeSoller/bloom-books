beb18f39c0103d290872964ca87189f70a5dcaf6
Felipe Soller <soller@outlook.com>
2023-06-04 22:06:33 -0300

    adds button to choose amount of items to be viewed

--- pages/index.js
+++ pages/index.js
@@ -10,15 +10,37 @@ import 'tailwindcss/tailwind.css';
 
 export default function Home({ genreLists }) {
   const [displayMode, setDisplayMode] = useState('list');
+  const [perPage, setPerPage] = useState(5);
 
   const handleDisplayMode = (mode) => {
     setDisplayMode(mode);
   };
 
+  const handlePerPage = (e) => {
+    setPerPage(parseInt(e.target.value));
+  };
+
+  const startIndex = perPage
+  const endIndex = startIndex + perPage;
+  const paginatedGenreLists = genreLists.slice(startIndex, endIndex);
+
   return (
     <div>
       <div className="flex flex-col sm:flex-row justify-between items-center bg-slate-100 p-2 px-5 sm:px-28">
         <div className="flex justify-between items-center mt-2 sm:mt-0">
+          <div className="mr-5">
+            <span className="mr-2">Exibir:</span>
+            <select
+              value={perPage}
+              onChange={handlePerPage}
+              className="px-4 py-2 rounded-lg bg-transparent"
+            >
+              <option value={5}>5</option>
+              <option value={10}>10</option>
+              <option value={15}>15</option>
+            </select>
+            <span className="ml-2">por vez</span>
+          </div>
           <div className="flex">
             <button
               onClick={() => handleDisplayMode('list')}
@@ -37,9 +59,9 @@ export default function Home({ genreLists }) {
       </div>
       <div className="mt-5 pl-28 pr-6">
         {displayMode === 'list' ? (
-          <GenreList genreLists={genreLists} />
+          <GenreList genreLists={paginatedGenreLists} />
         ) : (
-          <GenreCardList genreLists={genreLists} />
+          <GenreCardList genreLists={paginatedGenreLists} />
         )}
       </div>
     </div>
